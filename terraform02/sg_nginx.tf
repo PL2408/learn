e33210979a24d18675c987042125f55edaa747ff
PL2408 <pt.lazarenko@gmail.com>
2022-11-12 13:56:43 +0200

    add jenkins skill

---
+++ terraform02/sg_nginx.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "ec2_nginx_sg" {
+  name        = "ec2_nginx_sg"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.visitka_vpc.id
+
+  ingress {
+    description = "SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.visitka_elb_sg.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ec2_nginx_sg"
+  }
+}
