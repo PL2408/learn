2920f0e63194cf7bc8391e7884316f966fb2ffb0
PL2408 <pt.lazarenko@gmail.com>
2024-01-30 12:58:01 +0200

    add new conspect

---
+++ scripts_learn/loops2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+num=$1
+who=$2
+whom=$3
+
+
+echo "$who gave $num apples to $whom"
+
+var2=$(echo $var1 | rev)
+
+echo "<h1>Hello $var2</h1>"
+echo "<h1>Hello ${2}</h1>"
+echo "<h1>Hello $3</h1>"
