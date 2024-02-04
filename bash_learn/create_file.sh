6c1e3998d940786927d5060366134d5cc5e8711e
PL2408 <pt.lazarenko@gmail.com>
2024-02-04 14:07:15 +0200

    modified create_file.sh

--- bash_learn/create_file.sh
+++ bash_learn/create_file.sh
@@ -1,7 +1,22 @@
-#!/bin/bash
+#!/bin/bash 
+set -x
 
-filename="$(date +'%Y%m%d_%H%M%S').txt"
+filename="$(date +'%Y%m%d_%H').txt"
 
-#echo $filename
+bn=$(basename $0)
 
-echo "$(date)" > "$filename"
+log_file="${bn%.*}.$(date +'%Y%m%d').log"
+
+echo "$bn: $(date +'%Y%m%d %Hh %Mm %Ss'): filename=$filename" >> $log_file
+
+_date="$(date +'%Y%m%d_%H%M%S')"
+
+echo "[LOG] Date: $_date:DEBUG: Some event" >> "$filename"
+
+echo "$bn: $(date +'%Y%m%d %Hh %Mm %Ss'): start computition" >> $log_file
+sleep 3
+echo "$bn: $(date +'%Y%m%d %Hh %Mm %Ss'): end computition" >> $log_file
+
+set +x
+
+exit 0
